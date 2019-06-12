e4e608bb450e18a403d222b80c50927d2e1ba435
extrsun <sunwei@pmo.ac.cn>
2019-06-13 02:12:47 +0800

    upadate the ionic fraction comparison plot

--- adiabatic/comp_plot_ionfrac.py
+++ adiabatic/comp_plot_ionfrac.py
@@ -30,17 +30,22 @@ nei_ionfrac = pickle.load(open(rootpath+'nei_case/tionfrac_nei.pkl','rb'))
 
 # Only compare the ionic fraction of C, N, O, Ne, Mg, and Si because of
 # the color table limit
-Zlist = [6,7,8,10,12,14]
+Zlist = [6,7,8,10,12,14,16,26]
 
 radius = conditions[0:ncondi]['R']/3.0856780e+18
 mpl.style.use('default')
 for Z in Zlist:
   elsymb = pyatomdb.atomic.Ztoelsymb(Z)
-  # ax = plt.figure()
-  for i in range(5,Z+1):
-    plt.plot(radius, nei_ionfrac[Z][i,0:ncondi], color='C'+"%s"%(i-5), \
+  if Z == 26:
+    begin_ion = 16
+    ion_range = [16,17,18,19,20,21,22]
+  else:
+    begin_ion = Z-5
+    ion_range = range(begin_ion,Z+1)
+  for i in ion_range:
+    plt.plot(radius, nei_ionfrac[Z][i,0:ncondi], color='C'+"%s"%(i-begin_ion), \
       linewidth=2, label=elsymb+' '+pyatomdb.atomic.int2roman(i+1))
-    plt.plot(radius, cie_ionfrac[Z][i,0:ncondi], color='C'+"%s"%(i-5), \
+    plt.plot(radius, cie_ionfrac[Z][i,0:ncondi], color='C'+"%s"%(i-begin_ion), \
       linewidth=2.5, linestyle='dashed')
   plt.plot(radius, np.zeros(ncondi), linewidth=2.5, \
     linestyle='dashed', label='CIE')
@@ -51,3 +56,4 @@ for Z in Zlist:
   plt.yscale('log')
   plt.legend(loc=0)
   plt.savefig(rootpath+'figures/comp_ionfrac/ionfrac_comp_%s.png' %elsymb)
+  plt.close()
